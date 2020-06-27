3e12be6a2e8ddaf9b9690e5c0f9d5b886e646d58
Sébastien RONDEAU CAMEIRA <2760742+noahseed@users.noreply.github.com>
2020-06-27 12:11:07 +0200

    Initial commit

---
+++ TopQuiz/app/src/main/java/fr/rondeaucameira/topquiz/model/User.java
@@ -0,0 +1,20 @@
+package fr.rondeaucameira.topquiz.model;
+
+public class User {
+    private String mFirstname;
+
+    public String getFirstname() {
+        return mFirstname;
+    }
+
+    public void setFirstname(String firstname) {
+        mFirstname = firstname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mFirstname='" + mFirstname + '\'' +
+                '}';
+    }
+}
